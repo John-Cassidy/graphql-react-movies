85ba1a81e571b1ce7d56118da74928c4ad2dffc7
John Cassidy <jpcassidy@outlook.com>
2022-02-03 10:49:03 -0500

    40 server: add Director GraphQL query type and test data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -3,27 +3,60 @@ import Movie from '../models/movie.js';
 import _ from 'lodash';
 import graphql from 'graphql';
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+} = graphql;
 
 // DEMO DATA
 const movies = [
   { name: 'Joker', genre: 'Drama', id: '1' },
   { name: 'Moonrise kingdom', genre: 'Romance', id: '2' },
-  { name: 'La La Land', genre: 'Musical', id: ' 3' },
+  { name: 'La La Land', genre: 'Musical', id: '3' },
   { name: 'Interstellar', genre: 'Schi-Fi', id: '4' },
 ];
+const directors = [
+  { name: 'Todd Philips', age: 60, id: '1' },
+  { name: 'Wes Anderson', age: 52, id: '2' },
+  { name: 'Damien Chazelle', age: 58, id: '3' },
+  { name: 'Christopher Nolan', age: 51, id: '4' },
+];
 
-// movie(id: '5'){
+// URL TO GraphiQL UI: http://localhost:5000/graphql
+
+// QUERIES FOR MOVIE, DIRECTOR
+// {
+//   movie(id:3){
 //     name
 //     genre
+//   }
+// }
+
+// {
+//   director(id:3){
+//     name
+//     age
+//   }
 // }
 
 const MovieType = new GraphQLObjectType({
   name: 'Movie',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+  }),
+});
+
+const DirectorType = new GraphQLObjectType({
+  name: 'Director',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
   }),
 });
 
@@ -32,13 +65,23 @@ const RootQuery = new GraphQLObjectType({
   fields: () => ({
     movie: {
       type: MovieType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        //get data from demo data
-        // console.log(_.find(movies, { id: args.id }));
+        // get data from demo data
+        console.log(_.find(movies, { id: args.id }));
         return _.find(movies, { id: args.id });
         // // get data from database
         // return Movie.findById(args.id);
+      },
+    },
+    director: {
+      type: DirectorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        // get data from demo data
+        return _.find(directors, { id: args.id });
+        // // get data from database
+        // return Director.findById(args.id);
       },
     },
   }),
