2f13db99e0ad5c176b0437ce343cdd9e4c16f8b9
John Cassidy <jpcassidy@outlook.com>
2022-02-05 10:35:56 -0500

    44 client: display movies on MovieList component

--- client/src/components/MovieList.js
+++ client/src/components/MovieList.js
@@ -1,9 +1,42 @@
+import { gql, useQuery } from '@apollo/client';
+
+const GET_MOVIES_QUERY = gql`
+  {
+    movies {
+      name
+      genre
+      id
+    }
+  }
+`;
+
 function MovieList() {
+  const { loading, data, error } = useQuery(GET_MOVIES_QUERY);
+
+  if (loading) return <p>Loading...</p>;
+  console.log(data);
+
+  const renderMovies = () => {
+    return data.movies.map((movie) => {
+      return (
+        <li
+          className='list-group-item m-2 shadow-sm '
+          style={{ cursor: 'pointer', borderRadius: '5px' }}
+          key={movie.id}
+        >
+          {movie.name}
+        </li>
+      );
+    });
+  };
+
   return (
-    <div className='App'>
-      <ul>
-        <li>Movie List</li>
-      </ul>
+    <div className='row h-100'>
+      <div className='col-md-7 p-4'>
+        <ul className='list-group list-group-horizontal flex-wrap'>
+          {renderMovies()}
+        </ul>
+      </div>
     </div>
   );
 }
